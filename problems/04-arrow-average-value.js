40be279a06c1bec2d96641fc89fc558b0df90597
Hiep Trinh <xmikecolx@gmail.com>
2022-05-19 15:07:52 -0500

    04 and 05 solved

--- problems/04-arrow-average-value.js
+++ problems/04-arrow-average-value.js
@@ -10,7 +10,15 @@ arrowAvgValue([2, 3, 7]); // => 4
 arrowAvgValue([100, 60, 64]); // => 74.66666666666667
 ***********************************************************************/
 
-// your code here!
+const arrowAvgValue = array => {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    let ele = array[i];
+    sum += ele;
+  }
+  // sum = sum/array.length;
+  return sum/ array.length;
+}
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
--- problems/05-arrow-mirror-array.js
+++ problems/05-arrow-mirror-array.js
@@ -10,7 +10,20 @@ arrowMirrorArray([1,2,3]); // => [ 1, 2, 3, 3, 2, 1 ]
 arrowMirrorArray(['a', 'b', 'c', 'd']); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a' ]
 ***********************************************************************/
 
-// your code here
+const arrowMirrorArray = array => {
+  // let newArr = [];
+  // for (let i = 0; i < array.length; i++) {
+  //   let ele = array[i];
+  //   newArr.push(ele);
+  // }
+  // for (let i = array.length - 1; i >= 0; i--) {
+  //   newArr.push(array[i]);
+  // }
+  // return newArr;
+  let copy = array.slice();
+  let reverse = copy.reverse();
+  return array.concat(reverse)
+}
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
